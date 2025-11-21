256f519db692f8b6ad2e6f1f2ed012a02b587bf8
Brandon Josue <brandonsalinas792@gmail.com>
2025-11-21 13:43:29 -0600

    .

--- src/modules/Contabilidad/Contabilidad/ActivosFijos/views/CreateUpdateFixedAssetView.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import FixedAssetForm from '@/modules/Contabilidad/Contabilidad/ActivosFijos/components/FixedAssetForm.vue'
-</script>
-
-<template>
-    <FixedAssetForm />
-</template>
--- src/modules/Contabilidad/Contabilidad/Dashboard/views/AccountingDashboardView.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import BaseTitle from '@/shared/components/BaseTitle.vue'
-</script>
-
-<template>
-    <BaseTitle title="Dashboard de Contabilidad" subtitle="Información general de contabilidad"/>
-</template>
